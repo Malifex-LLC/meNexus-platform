278193d84a97418cfa46d993f192844c29f85f40
enki <jacobwileyross@gmail.com>
2024-12-21 19:44:59 -0600

    MENEX-63: Add NotificationsTray Component

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,8 +1,17 @@
 import './Header.css';
 import '../Search/Search.jsx'
+import { useState } from "react";
+import NotificationsTray from '../Notifications/NotificationsTray/NotificationsTray.jsx'
 import Search from "../Search/Search.jsx";
 
 const Header = () => {
+
+    const [showNotificationsTray, setShowNotificationsTray] = useState(false);
+
+    const toggleNotificationsTray = () => {
+        setShowNotificationsTray((prevState) => !prevState);
+    }
+
     return (
         <div className="header__container">
             <header
@@ -13,7 +22,22 @@ const Header = () => {
                 <h1>meNexus</h1>
             </header>
             <div className="header__search">
-                <Search />
+                <Search/>
+            </div>
+            <div className="header__notifications">
+                <p
+                    className={`header__notifications-tray-toggle ${
+                        showNotificationsTray ? 'header__notifications-tray-toggle--active' : ''
+                    }`}
+                    onClick={toggleNotificationsTray}
+                >
+                    Notifications
+                </p>
+                {showNotificationsTray && (
+                    <div className="header__notifications-tray">
+                        <NotificationsTray/>
+                    </div>
+                )}
             </div>
         </div>
     )
