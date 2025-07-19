f5e092c272211e05d302f680e5ef3b4d890a707d
enki <jacobwileyross@gmail.com>
2025-07-19 14:03:37 -0500

    Updated express.js to pull CORS_ORIGIN from .env

--- services/synapse/src/api/config/express.js
+++ services/synapse/src/api/config/express.js
@@ -7,13 +7,16 @@ import express  from 'express'
 // Import CORS library
 import cors from 'cors'
 
+import dotenv from 'dotenv';
+dotenv.config();
+
 // Configure and instantiate Express app
 export function createExpressApp () {
     const app = express();
 
     // Configure CORS
     app.use(cors({
-        origin: 'http://localhost:5173', // web's origin
+        origin: process.env.CORS_ORIGIN, // web's origin
         credentials: true, // Allow credentials (cookies) to be sent
     }));
 
