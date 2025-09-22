ef81865831acf6145008de8a8ff99ea768327e18
enki <jacobwileyross@gmail.com>
2025-09-22 01:45:31 -0500

    update handleData to bypass HTTP layer for create post

--- services/synapse/src/handlers/handleData.js
+++ services/synapse/src/handlers/handleData.js
@@ -8,6 +8,7 @@ import { sendRequest, replaceParams } from "#utils/apiUtils.js";
 import { ENDPOINTS } from "#api/config/endpoints.js";
 import { resolvePendingRequest } from "#core/messenger.js";
 import * as peerStateManager from '#core/peerStateManager.js';
+import * as postService from '#api/models/post.js';
 import FormData from 'form-data';
 
 export const handleData = async (libp2p, message) => {
@@ -405,11 +406,7 @@ export const handleData = async (libp2p, message) => {
                         console.log(`Received Create POST request from ${message.meta.sender}.`);
                         console.log("Received Create POST payload:", message.payload);
                         const { publicKey, activeBoard, content } = message.payload;
-                        const response = await sendRequest({
-                            method: 'POST',
-                            url: ENDPOINTS.CREATE_POST,
-                            data: {publicKey, activeBoard, content},
-                        });
+                        const response = await postService.createPost(publicKey, activeBoard, content);
                         //console.log("CREATE_POST response ", response);
                         const post = response.data;
 
