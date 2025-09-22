be41ec17f2b746adc519090f6d489d08e7e066f3
enki <jacobwileyross@gmail.com>
2025-09-22 03:07:07 -0500

    fix commentServices.js

--- services/synapse/src/api/services/commentServices.js
+++ services/synapse/src/api/services/commentServices.js
@@ -1,5 +1,4 @@
 import Comment from '../models/comment.js';
-import * as commentService from "#api/models/comment.js";
 
 export const createComment = async (resourceType, resourceId, content, publicKey) => {
     return await Comment.createComment(resourceType, resourceId, content, publicKey);
@@ -20,5 +19,6 @@ export const getComments = async (resourceType, resourceId) => {
 export default {
     createComment,
     updateComment,
-    deleteComment
+    deleteComment,
+    getComments
 }
