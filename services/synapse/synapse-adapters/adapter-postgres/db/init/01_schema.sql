ee3417a290f926f24033206380a36192dcb828c8
enki <jacobwileyross@gmail.com>
2025-11-03 13:41:39 -0600

    add db init script

--- services/synapse/synapse-adapters/adapter-postgres/db/init/01_schema.sql
+++ services/synapse/synapse-adapters/adapter-postgres/db/init/01_schema.sql
@@ -1,8 +1,7 @@
--- Add up migration script here
 CREATE EXTENSION IF NOT EXISTS pgcrypto;
 
 CREATE TABLE IF NOT EXISTS public.events (
   event_id uuid PRIMARY KEY DEFAULT gen_random_uuid(),
   event_type text NOT NULL,
-  agent_public_key text NOT NULL
+  agent_public_key text NOT NULL,
 );
--- services/synapse/synapse-adapters/adapter-postgres/migrations/20251102085338_init_schema.down.sql
+++
@@ -1,4 +0,0 @@
--- SPDX-License-Identifier: AGPL-3.0-or-later
--- Copyright © 2025 Malifex LLC and contributors
-
--- Add down migration script here
